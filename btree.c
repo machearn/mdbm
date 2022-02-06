5ea5af61a73fe8c3e34c8983fb4d975dcdaf3d2e
machearn <machearn.n@gmail.com>
2022-02-06 13:34:56 -0500

    implement binary search in one Page

--- btree.c
+++ btree.c
@@ -20,3 +20,46 @@ int loadPage(int fd, off_t offset, Page** pPage) {
     }
     return 0;
 }
+
+off_t searchInternalNode(Page* node, uint64_t key) {
+    uint8_t size = node->numCells;
+    uint8_t left = 0;
+    uint8_t right = size;
+
+    if (key < node->cells[left].key) return node->leftMost;
+    if (key >= node->cells[right].key) return node->cells[size-1].offset;
+
+    while (left < right) {
+        uint8_t mid = left + (right - left) / 2;
+        if (node->cells[mid].key <= key) {
+            left = mid+1;
+        }
+        else {
+            right = mid;
+        }
+    }
+
+    return node->cells[left-1].offset;
+}
+
+int searchLeafNode(Page* node, uint64_t key, Cell* cell) {
+    uint8_t size = node->numCells;
+    uint8_t left = 0;
+    uint8_t right = size;
+
+    while (left < right) {
+        uint8_t mid = left + (right - left) / 2;
+        if (node->cells[mid].key == key) {
+            memcpy((void*)((node->cells)+mid), (void*)cell, sizeof(Cell));
+            return 0;
+        }
+        else if (node->cells[mid].key > key) {
+            right = mid;
+        }
+        else {
+            left = mid+1;
+        }
+    }
+
+    return -1;
+}
