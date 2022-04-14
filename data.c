5be044a5339c31ba07ec2a7c55c3316c4a03b0d1
machearn <machearn.n@gmail.com>
2022-04-14 15:22:10 -0400

    add `payload tail` to `data page`, rewrite the APIs

--- data.c
+++ data.c
@@ -36,83 +36,104 @@ ssize_t load_data_page(int fd, DataPage* data_page, off_t offset) {
     return ret;
 }
 
-size_t get_tuple_size(DataPage* page, size_t slot_index) {
-    if (slot_index >= page->num_tuples) return 0;
+ssize_t init_data_page(DataPage* data_page) {
+    data_page->num_tuples = 0;
+    data_page->offset = -1;
+    memset(data_page->data, 0, PAYLOAD_SIZE);
+    return 0;
+}
 
-    uint16_t* slot = (uint16_t*)page->data;
-    size_t tail = 4096;
-    if (slot_index > 0)
-        tail = slot[slot_index - 1];
+ssize_t add_data_page(int fd, void* tuple, size_t tuple_size) {
+    DataPage* data_page = malloc_data_page();
+    init_data_page(data_page);
+    int16_t* slot = (int16_t*)data_page->data;
+    int16_t tail = 4096;
+    int16_t offset = (int16_t)(tail - tuple_size);
 
-    return tail - slot[slot_index];
+    memcpy(data_page->data + offset, tuple, tuple_size);
+    slot[0] = offset;
+    data_page->num_tuples++;
+
+    if ((data_page->offset = lseek(fd, 0, SEEK_END)) < 0) {
+        free_data_page(&data_page);
+        return -1;
+    }
+
+    ssize_t ret = dump_data_page(fd, data_page);
+    off_t page_offset = data_page->offset;
+    free_data_page(&data_page);
+    return ret < 0 ? -1 : page_offset;
 }
 
 ssize_t insert_data(int fd, DataPage* page, off_t page_offset, char* tuple, size_t tuple_size) {
     load_data_page(fd, page, page_offset);
 
-    uint16_t* slot = (uint16_t*)page->data;
-    uint16_t tail = 4096;
+    int16_t* slot = (int16_t*)page->data;
+    int16_t tail = 4096;
     int i = 0;
     for (i = 0; i < page->num_tuples; i++) {
         if (slot[i] > 0) tail = slot[i];
         else break;
     }
 
-    uint16_t tuple_offset = tail - tuple_size;
-    if (page->data[tuple_offset] != 0) return -1;
+    int16_t tuple_offset = (int16_t)(tail - tuple_size);
+    if (page->data[tuple_offset] != 0) {
+        return add_data_page(fd, tuple, tuple_size);
+    }
     memcpy(page->data + tuple_offset, tuple, tuple_size);
     slot[i] = tuple_offset;
     page->num_tuples++;
 
-    return dump_data_page(fd, page);
+    ssize_t ret = dump_data_page(fd, page);
+    return ret < 0 ? -1 : page_offset;
 }
 
-ssize_t fetch_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, void* tuple) {
+ssize_t fetch_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, void* tuple, size_t tuple_size) {
     load_data_page(fd, page, page_offset);
     if (slot_index >= page->num_tuples) return -1;
 
-    uint16_t* slot = (uint16_t*)page->data;
-    size_t tuple_size = get_tuple_size(page, slot_index);
-    uint16_t tuple_offset = slot[slot_index];
+    int16_t* slot = (int16_t*)page->data;
+    int16_t tuple_offset = slot[slot_index];
 
     memcpy(tuple, page->data + tuple_offset, tuple_size);
     return 0;
 }
 
-ssize_t update_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, char* new_tuple, size_t tuple_size) {
+ssize_t update_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, size_t old_tuple_size, char* new_tuple, size_t tuple_size) {
     load_data_page(fd, page, page_offset);
     if (slot_index >= page->num_tuples) return -1;
 
-    uint16_t* slot = (uint16_t*)page->data;
-    size_t old_tuple_size = get_tuple_size(page, slot_index);
-    uint16_t offset = slot[slot_index];
+    int16_t* slot = (int16_t*)page->data;
+    int16_t offset = slot[slot_index];
 
-    if (tuple_size <= old_tuple_size) {
+    if (tuple_size == old_tuple_size) {
         memcpy(page->data + offset, new_tuple, tuple_size);
     } else {
         memset(page->data + offset, 0, old_tuple_size);
-        uint16_t tail = slot[page->num_tuples - 1];
-        uint16_t new_offset = tail - tuple_size;
-        if (page->data[new_offset] != 0) return -1;
+        int16_t new_offset = (int16_t)(page->payload_tail - tuple_size);
+
+        if (page->data[new_offset] != 0) {
+            return add_data_page(fd, new_tuple, tuple_size);
+        }
         memcpy(page->data + new_offset, new_tuple, tuple_size);
         slot[slot_index] = new_offset;
     }
-    return dump_data_page(fd, page);
+    ssize_t ret = dump_data_page(fd, page);
+    return ret < 0 ? -1 : page_offset;
 }
 
-ssize_t delete_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index) {
+ssize_t delete_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, size_t tuple_size) {
     load_data_page(fd, page, page_offset);
     if (slot_index >= page->num_tuples) return -1;
 
-    uint16_t* slot = (uint16_t*)page->data;
-    size_t tuple_size = get_tuple_size(page, slot_index);
-    uint16_t offset = slot[slot_index];
+    int16_t* slot = (int16_t*)page->data;
+    int16_t offset = slot[slot_index];
 
     memset(page->data + offset, 0, tuple_size);
-    for (size_t i = slot_index; i < page->num_tuples - 1; i++) {
-        slot[i] = slot[i + 1];
+    if (offset == page->payload_tail) {
+        page->payload_tail = (int16_t) (offset - tuple_size);
     }
-    slot[page->num_tuples - 1] = 0;
+    slot[slot_index] = -1;
     page->num_tuples--;
 
     return dump_data_page(fd, page);
--- data.h
+++ data.h
@@ -7,22 +7,23 @@
 
 #include <sys/types.h>
 
+#define PAYLOAD_SIZE 4072
+
 typedef struct DataPage DataPage;
 
 struct DataPage {
     off_t offset;
     size_t num_tuples;
-    char data[4080];
+    int16_t payload_tail;
+    char data[PAYLOAD_SIZE];
 };
 
 DataPage* malloc_data_page();
 void free_data_page(DataPage** page);
 
-size_t get_tuple_size(DataPage* page, size_t slot_index);
-
 ssize_t insert_data(int fd, DataPage* page, off_t page_offset, char* tuple, size_t tuple_size);
-ssize_t fetch_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, void* tuple);
-ssize_t update_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, char* new_tuple, size_t tuple_size);
-ssize_t delete_tuple(int fd, DataPage* page, off_t page_offset, size_t slot);
+ssize_t fetch_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, void* tuple, size_t tuple_size);
+ssize_t update_tuple(int fd, DataPage* page, off_t page_offset, size_t slot_index, size_t old_tuple_size, char* new_tuple, size_t tuple_size);
+ssize_t delete_tuple(int fd, DataPage* page, off_t page_offset, size_t slot, size_t tuple_size);
 
 #endif //MDBM_DATA_H
